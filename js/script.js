3da1178b77926b22ea0c63f23a5433a8c9dd2e20
CosJames <51408660+CosJames@users.noreply.github.com>
2020-02-03 23:37:22 +0800

    Added To Repo

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(document).on('click','.card',function() {
+    $('.alert').alert();
+    //lipat tay
+    //wdym?
+    // cos mabagal ba net niyo?
+  });
+});
+
+
