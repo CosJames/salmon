55f8a9f1f08201f89aad39477c04a1a2bfe7d211
CosJames <51408660+CosJames@users.noreply.github.com>
2020-02-04 00:16:53 +0800

    Basic functionalities Like Prompts

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
-  $(document).on('click','.card',function() {
-    $('.alert').alert();
-    //lipat tay
-    //wdym?
-    // cos mabagal ba net niyo?
+  // Prompt when the card is clicked
+  $(document).on('click','#modal-ok',function() {
+    // TODO: Template, migrate to php next time
+    $("#alerts-container").append(`
+      <div class="alert alert-success alert-dismissible fade show">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <strong>Success!</strong> This alert box could indicate a successful or positive action.
+      </div>
+    `);
   });
 });
 
